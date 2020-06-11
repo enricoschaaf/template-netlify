aeb62b388a4e28c9f189e298c3ad58541e15e9c2
Enrico <54645197+enricoschaaf@users.noreply.github.com>
2020-06-11 09:42:46 +0200

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  reactStrictMode: true,
+  experimental: {
+    modern: true,
+    polyfillsOptimization: true
+  },
+  webpack(config, { dev, isServer }) {
+    const splitChunks = config.optimization && config.optimization.splitChunks
+    if (splitChunks) {
+      const cacheGroups = splitChunks.cacheGroups
+      const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/
+      if (cacheGroups.framework) {
+        cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
+          test: preactModules
+        })
+        cacheGroups.commons.name = "framework"
+      } else {
+        cacheGroups.preact = {
+          name: "commons",
+          chunks: "all",
+          test: preactModules
+        }
+      }
+    }
+
+    const aliases = config.resolve.alias || (config.resolve.alias = {})
+    aliases.react = aliases["react-dom"] = "preact/compat"
+    if (dev && !isServer) {
+      const entry = config.entry
+      config.entry = () =>
+        entry().then(entries => {
+          entries["main.js"] = ["preact/debug"].concat(entries["main.js"] || [])
+          return entries
+        })
+    }
+
+    return config
+  }
+}
